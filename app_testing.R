842d9258da8cbd6327b5b184eb5b1625ec62fef4
Hallee Shearer <halleeshearer@outlook.com>
2024-03-25 14:35:32 -0400

    coloring of CI plot

--- app_testing.R
+++ app_testing.R
@@ -49,6 +49,7 @@ options(spinner.color = "#9ecadb",
         
 # helper function for plotting:
 plot_sim_ci <- function(data, name) {
+
   # remove na
   na_idx <- is.na(data$d) | is.na(data$sim_ci_lb) | is.na(data$sim_ci_ub)
   data$d <- data$d[!na_idx]
@@ -59,17 +60,44 @@ plot_sim_ci <- function(data, name) {
   sorted_upper_bounds <- data$sim_ci_ub[sorted_indices]
   sorted_lower_bounds <- data$sim_ci_lb[sorted_indices]
   
-  # downsample the data for plotting
-  # downsample_factor <- 1000
-  # sorted_d <- sorted_d[seq(1, length(sorted_d), downsample_factor)]
-  # sorted_upper_bounds <- sorted_upper_bounds[seq(1, length(sorted_upper_bounds), downsample_factor)]
-  # sorted_lower_bounds <- sorted_lower_bounds[seq(1, length(sorted_lower_bounds), downsample_factor)]
-   plot(sorted_d, type = "l", ylim = c(min(sorted_lower_bounds, na.rm = TRUE), max(sorted_upper_bounds, na.rm = TRUE)),
+  # for coloring of confidence intervals:
+  below_zero <- sorted_upper_bounds < 0
+  below_cross_idx <- which(diff(below_zero) == -1) # the last TRUE before switch
+  
+  above_zero <- sorted_lower_bounds > 0
+  above_cross_idx <- (which(diff(above_zero) == 1)) + 1 # the last FALSE before switch to true
+  
+  # if there are no values below zero, set the index to 1
+  if (length(below_cross_idx) == 0) {
+    below_cross_idx = 1
+  } 
+  
+  # if there are no values above zero, set the index to the end
+  if (length(above_cross_idx) == 0) {
+    above_cross_idx = length(above_zero)
+  } 
+
+  # could downsample for plotting if slow
+
+  # plot a line for d
+  plot(sorted_d, type = "l", ylim = c(min(sorted_lower_bounds, na.rm = TRUE), max(sorted_upper_bounds, na.rm = TRUE)),
        main = name, xlab = "Edges/Voxels", ylab = "Cohen's d")
   
-  polygon(c(1:length(sorted_d), rev(1:length(sorted_d))), 
-          c(sorted_upper_bounds, rev(sorted_lower_bounds)), 
-          col = rgb(0.5, 0.5, 0.5, alpha = 0.3), border = NA)
+  # plot and shade the cofidence intervals:
+  # green for intervals that are entirely below zero
+  polygon(c(1:below_cross_idx, rev(1:below_cross_idx)), 
+          c(sorted_upper_bounds[1:below_cross_idx], rev(sorted_lower_bounds[1:below_cross_idx])), 
+          col = rgb(177/255, 207/255, 192/255, alpha = 0.5), border = NA)
+  
+  # red for intervals that include zero
+  polygon(c(below_cross_idx:above_cross_idx, rev(below_cross_idx:above_cross_idx)), 
+          c(sorted_upper_bounds[below_cross_idx:above_cross_idx], rev(sorted_lower_bounds[below_cross_idx:above_cross_idx])), 
+          col = rgb(237/255, 185/255, 185/255, alpha = 0.5), border = NA)
+  
+  # green for intervals that are entirely above zero
+  polygon(c(above_cross_idx:length(above_zero), rev(above_cross_idx:length(above_zero))), 
+          c(sorted_upper_bounds[above_cross_idx:length(above_zero)], rev(sorted_lower_bounds[above_cross_idx:length(above_zero)])), 
+          col = rgb(177/255, 207/255, 192/255, alpha = 0.5), border = NA)
 }
 
 
@@ -106,15 +134,15 @@ ui <- fluidPage(
       
       selectInput("test_type",
       			  label = "Test Type",
-      			  choices = c("All" = "*", "One-sample task-rest" = "\\.t\\.", "Two-sample group contrast" = "\\.t2\\.", "Behavioural correlation" = "\\.r\\.")), ## TODO: change this is d when data is updated to cohen's d
+      			  choices = c("All" = "*", "One-sample task-rest" = "t", "Two-sample group contrast" = "t2", "Behavioural correlation" = "r")), ## TODO: change this is d when data is updated to cohen's d
       			  
       conditionalPanel(
         condition = "input.test_type.indexOf('r') > -1",
             selectInput("behaviour",
       			  label = "Behavioural correlation", #TODO: update choices to include all possible options! For example, missing BMI currently
-      			  choices = unique(study[study$orig_stat_type=="r","var2"]),
-              multiple = TRUE),
-              selected = "*"),
+      			  choices = c("All" = "*", unique(study[study$orig_stat_type=="r","var2"])),
+              multiple = TRUE,
+              selected = c("*"))),
       # selectInput("behaviour",
       # 			  label = "Behavioural correlation",
       # 			  choices = c( "All" = "*","Age" = "\\.age", "IQ" = "\\.iq", "Fluid Intelligence" = "\\.gf", "Peabody Picture Vocab Test" = "\\.ppvt", "Expressive Vocab Test" = "\\.evt", "Stop Signal Task" = "\\.SST", "Letter N-Back Accuracy" = "\\.lnbxacc", "Letter N-Back Response Time" = "\\.lnbxrt", "Penn Face Memory Test Accuracy" = "\\.pfmtxacc", "Penn Face Memory Test Response Time" = "\\.pfmtxrt", "Penn Matrix Reasoning Test Correct Responses" = "\\.pmatxrc", "Penn Verbal Reasoning Test Accuracy" = "\\.pvrtxacc", "Penn Verbal Reasoning Test Response Time" = "\\.pvrtxrt", "Penn Word Memory Test Accuracy" = "\\.pwmtxacc", "Penn Word Memory Test Response Time" = "\\.pwmtxrt", "Wide Range Assessment Test" = "\\.wrat"),
@@ -179,11 +207,11 @@ server <- function(input, output, session) {
         #                         ifelse(input$test_type == "\\.r\\.", study$var2 == input$behaviour, TRUE))]
 
 
-        v$d_clean <- d_clean[(grepl(input$dataset, study$dataset) & 
+        v$d_clean <- d_clean[grepl(input$dataset, study$dataset) & 
                              grepl(input$measurement_type, study$map_type) & 
-                             grepl(input$task, study$var1) & 
+                             (length(input$task) == 0 | grepl(input$task, study$var1)) & 
                              grepl(input$test_type, study$orig_stat_type) & 
-                             ifelse(input$test_type == "\\.r\\.", grepl(input$behaviour, study$var2), TRUE))]
+                             grepl(paste(input$behaviour, collapse="|"), study$var2)]
       
 
     # output$selected_vars <- renderText({
@@ -334,7 +362,7 @@ server <- function(input, output, session) {
       else {
         plot_output_list <- lapply(1:length(v$d_clean), function(i) {
           plotname <- paste0("plot", i)
-          plotOutput(plotname)
+          plotOutput(plotname, height = "250px")
         })
 
         # convert the list to a tagList, this is necessary for the list of items to display properly
