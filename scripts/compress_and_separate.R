3656d27c4d720b8523d53cc46a6dcb8f3b3a9669
Hallee Shearer <halleeshearer@outlook.com>
2025-02-18 17:29:43 -0500

    testing

---
+++ scripts/compress_and_separate.R
@@ -0,0 +1,36 @@
+# make 12 separate RData files, one for each combo
+
+# load combined data
+load('combined_data_2025-02-13.RData')
+
+# get list of combo names
+combo_names = unique(names(data$abcd_fc_r_REST_age_months))
+# make multivariate names generalizable to all tests with wildcard
+combo_names = sub("multi.r", "multi.*",x = combo_names)
+
+
+# loop through combo_names
+for (combo in combo_names) {
+  # for each study, grab that combo's data
+  this_combo_data = list()
+  for (name in names(data)) {
+    this_combo = names(data[[name]])[grepl(combo, names(data[[name]]))]
+    combo_data = data[[name]][[this_combo]]
+    # only keep n, n1, n2, d, ci, r_sq, ci
+    combo_data$stat = list()
+    combo_data$p = list()
+    combo_data$std.brain = list()
+    combo_data$std.score = list()
+    this_combo_data[[name]] = combo_data
+  }
+  # save this_combo_data as RData file
+  save(combo_data, file = paste0('combo_', combo, '.RData'))
+}
+
+d = data
+for (name in names(data)) {
+  for (combo in names(data[[name]])) {
+    data[[name]][[combo]]$sim_ci_lb = unlist(data[[name]][[combo]]$sim_ci_lb)
+    data[[name]][[combo]]$sim_ci_ub = unlist(data[[name]][[combo]]$sim_ci_ub)
+  }
+}
